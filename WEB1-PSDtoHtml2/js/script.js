76dbd5da60b68ffbef2d24322b99354204cfa66e
Jean Brown <jeantoruno00@gmail.com>
2020-10-07 00:51:09 -0600

    Manejo de eventos con clases desde JS

---
+++ WEB1-PSDtoHtml2/js/script.js
@@ -0,0 +1,20 @@
+//Este proceso llama al div que contiene la ventana
+const ventanaModal = document.getElementsByClassName("modal");
+
+//Este proceso llama al boton para abrir el modal del peronaje Nissa.
+const boton = document.getElementsByClassName("botton");
+
+//cierra la ventana de Nissa.
+const cierre = document.getElementsByClassName("cerrar")[0];
+
+// Se oculta la ventana modal desde JS
+ventanaModal[0].classList.add('ocultar');
+
+//Muesta el Modal
+boton[0].addEventListener('click',()=>{
+    ventanaModal[0].classList.remove('ocultar');
+})
+  //cierra el modal cuando se toca el boton de cierre.
+cierre.addEventListener('click',()=>{
+    ventanaModal[0].classList.add('ocultar');
+})
